ba10f885f6ed67f7147124b3f380e0e6a4ed91d3
EunSeong-Park <49016892+EunSeong-Park@users.noreply.github.com>
2021-12-19 22:53:32 +0900

    .

--- common.py
+++ common.py
@@ -15,11 +15,14 @@ MODE_TITLE = 0
 MODE_GAME = 1
 MODE_INGAME_MENU = 2
 
+DISABLED = -1
+
 SIZE_SCREEN = (800, 800)
 SIZE_GAMESCREEN = (800, 600)
 RECT_GAMESCREEN = (0, 0, 800, 600)
 RECT_TERMINAL = (0, 600, 600, 200)
 RECT_PROMPT = (0, 780, 600, 20)
+RECT_TXT_PROMPT = (0, 785, 600, 12)
 SIZE_ICON = (40, 40)
 
 BLACK = (0, 0, 0)
@@ -29,16 +32,21 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+MAX_TERMINAL_LINES = 15
+
 MAP_XP_VILLAGE = 0
 
 RECT_START = (300, 650, 200, 50)
 
 DIR_IMAGE = "asset/image/"
 DIR_FONT = "asset/font/"
+DIR_DATA = "data/"
+
+PREFIX_TERMINAL = "player@game:"
 
 TIMER_TICK = 60
 
-DEF_SPEED = 3
+DEF_SPEED = 11
 
 ##################
 # PYGAME OBJECTS #
@@ -57,7 +65,13 @@ FONT_CONSOLAS_20 = pygame.font.Font(DIR_FONT + "consolas.ttf", 20)
 FONT_BASIC_40 = pygame.font.Font(DIR_FONT + "malgun.ttf", 40)
 FONT_BOLD_40 = pygame.font.Font(DIR_FONT + "malgunbd.ttf", 40)
 
-# common functions
+
+####################
+# COMMON FUNCTIONS #
+####################
+
+# pos: (x,y)
+# rect: (x,y,w,h)
 def isin(pos, rect):
     return pos[0] >= rect[0] and \
             pos[1] >= rect[1] and \
@@ -69,3 +83,8 @@ def isin(pos, rect):
 def get_pos_center_aligned_text(text, rect):
     return ((rect[0] + rect[2] // 2) - text.get_width() // 2, 
             (rect[1] + rect[3] // 2) - text.get_height() // 2)
+
+# string: str
+def parse(string):
+    return [i for i in string.split(' ') if i != '']
+
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import json
+
+from pprint import pprint
+
 from common import *
 
 
@@ -5,9 +9,76 @@ screen = pygame.display.set_mode(SIZE_SCREEN)
 clock = pygame.time.Clock()
 
 rect_ch = pygame.Rect(0, 0, SIZE_ICON[0], SIZE_ICON[1])
+stat = dict()
+fs = dict()
+cur_dir = ["", ""]
+log = ["",]
+
+
+def cmd_cd(argv):
+    if len(argv) == 1:
+        return
+
+    dest = argv[1]
+    if dest == "..":
+        if len(cur_dir) == 2:
+            if cur_dir[-1] == "":
+                return
+            else:
+                cur_dir[-1] = ""
+                return
+        else:
+            cur_dir.pop()
+    else:
+        pass
+
+def cmd_exit():
+    global stat
+    stat['exp'] += 1
+
+    with open(DIR_DATA + "stat.json", "w") as f:
+        json.dump(stat, f, indent=4)
+
+    pygame.quit()
+    quit()
+
+def handler(argv):
+    if len(argv) == 0:
+        return 
+
+    cmd = argv[0]
+    if cmd not in stat['bin']: # TODO
+       log.append("%s: command not found" % cmd)
+
+
+    if cmd == "exit":
+        cmd_exit()
+    elif cmd == "ls":
+        pass
+    elif cmd == "man":
+        pass
+    elif cmd == "cat":
+        pass
+
+    elif cmd == "cd":
+        cmd_cd(argv)
+
+            
+
 
 def main_init():
-    pass
+    global stat
+    global fs
+
+    with open(DIR_DATA + "stat.json") as f:
+        stat = json.load(f)
+        pprint(stat)
+    
+    with open(DIR_DATA + "fs.json") as f:
+        fs = json.load(f)
+        pprint(fs)
+
+
 
 def main_menu():
     while True:
@@ -17,6 +88,9 @@ def main_menu():
             if e.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+
+            if e.type == pygame.KEYDOWN and e.key == pygame.K_RETURN:
+                return MODE_GAME
         
             if e.type == pygame.MOUSEBUTTONDOWN:
                 if isin(pygame.mouse.get_pos(), RECT_START):
@@ -36,9 +110,12 @@ def main_menu():
         clock.tick(TIMER_TICK)
 
 def main_game():
+    global log
+
     delta = [0, 0]
     terminal_active = False
     img_bg = IMGS_BG[0]
+    buffer = " "
 
     while True:
         # load map info
@@ -51,8 +128,7 @@ def main_game():
         
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
-                pygame.quit()
-                quit()
+                cmd_exit()
 
             if e.type == pygame.KEYDOWN:
                 # move
@@ -64,13 +140,22 @@ def main_game():
                     delta[1] -= DEF_SPEED
                 if e.key == pygame.K_DOWN:
                     delta[1] += DEF_SPEED
-            
-                # terminal activation
-                if terminal_active == False:
-                    terminal_active = True
+
+                if terminal_active:
+                    if e.key == pygame.K_BACKSPACE:
+                        buffer = buffer[:-1] if buffer != " " else " "
+                    elif e.key == pygame.K_RETURN:
+                        if buffer != " ":
+                            log.append(PREFIX_TERMINAL + "/" + buffer)
+                            handler(parse(buffer))
+                            buffer = ' '
+                        else:
+                            terminal_active = False
+                    else:
+                        buffer += e.unicode
                 else:
-                    # submit the command
-                    terminal_active = False # remove this, later
+                    if e.key == pygame.K_RETURN:
+                        terminal_active = True
 
 
             if e.type == pygame.KEYUP:
@@ -84,14 +169,20 @@ def main_game():
                 if e.key == pygame.K_DOWN:
                     delta[1] -= DEF_SPEED
         
-        rect_ch.move_ip(delta)
+        if isin((rect_ch.x+delta[0], rect_ch.y+delta[1]), 
+                 (0, 0, SIZE_GAMESCREEN[0]-SIZE_ICON[0], SIZE_GAMESCREEN[1]-SIZE_ICON[1])):
+            rect_ch.move_ip(delta)
 
-        # TODO: multiline text -> iteration
-        txt_terminal = FONT_CONSOLAS_12.render("$ lsYMSDMD!@02012,f exit", True, WHITE)
-
-
-        screen.blit(txt_terminal, RECT_TERMINAL)
+        # terminal
+        for i in range(MAX_TERMINAL_LINES):
+            txt_terminal = FONT_CONSOLAS_12.render(
+                log[max(0, len(log) - MAX_TERMINAL_LINES + i)], True, WHITE)
+            screen.blit(txt_terminal, (0, 600+12*i))
         
+        # prompt
+        txt_prompt = FONT_CONSOLAS_12.render(PREFIX_TERMINAL + "/" + "$" + buffer, True, WHITE)
+        screen.blit(txt_prompt, RECT_TXT_PROMPT)
+
         # footer
         pygame.draw.rect(screen, WHITE, RECT_TERMINAL, 1)
         pygame.draw.rect(screen, WHITE if terminal_active else GRAY, RECT_PROMPT, 1)
@@ -122,3 +213,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
